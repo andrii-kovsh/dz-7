3da01f213b72f8e876e972a54e68cb5be5a6a818
Andrii Kovsh <65176277+andrii-kovsh@users.noreply.github.com>
2023-07-17 17:48:48 +0300

    Update Output.java with variable name to chosenPlanet

--- src/main/java/Output.java
+++ src/main/java/Output.java
@@ -1,12 +1,12 @@
 public class Output {
         public static void main(String[] args) {
-                SolarSystem choosePlanet = SolarSystem.MARS;
+                SolarSystem chosenPlanet = SolarSystem.MARS;
                 System.out.println("Планета: " + choosePlanet);
-                System.out.println("Порядковий номер від сонця: " + choosePlanet.getOrderFromSun());
-                System.out.println("Відстань від сонця: " + choosePlanet.getDistanceFromSun() + " млн км");
-                System.out.println("Відстань від попередньої планети: " + choosePlanet.getDistanceFromPrevious() + " млн км");
-                System.out.println("Радіус: " + choosePlanet.getRadius() + " км");
-                System.out.println("Попередня планета: " + (choosePlanet.getPreviousPlanet() != null ? choosePlanet.getPreviousPlanet() : "немає"));
-                System.out.println("Наступна планета: " + (choosePlanet.getNextPlanet() != null ? choosePlanet.getNextPlanet() : "немає"));
+                System.out.println("Порядковий номер від сонця: " + chosenPlanet.getOrderFromSun());
+                System.out.println("Відстань від сонця: " + chosenPlanet.getDistanceFromSun() + " млн км");
+                System.out.println("Відстань від попередньої планети: " + chosenPlanet.getDistanceFromPrevious() + " млн км");
+                System.out.println("Радіус: " + chosenPlanet.getRadius() + " км");
+                System.out.println("Попередня планета: " + (chosenPlanet.getPreviousPlanet() != null ? chosenPlanet.getPreviousPlanet() : "немає"));
+                System.out.println("Наступна планета: " + (chosenPlanet.getNextPlanet() != null ? chosenPlanet.getNextPlanet() : "немає"));
         }
 }
